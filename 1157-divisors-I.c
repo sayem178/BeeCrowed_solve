41633fd8b9f16183bf90e68912025066aee34592
sayem178 <musaddiksayem@gmail.com>
2025-06-24 20:36:47 +0600

    formated

--- 1157-divisors-I.c
+++ 1157-divisors-I.c
@@ -7,9 +7,9 @@ int main()
 
     scanf("%i", &n);
 
-    for (i=1; i<=n; ++i)
+    for (i = 1; i <= n; ++i)
     {
-        if (n%i==0)
+        if (n % i == 0)
             printf("%i\n", i);
     }
 
